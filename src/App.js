617224903eb62bf6b1c25b2d4231dbce49520a84
Cristian <cristian.candidatu@itom.ro>
2023-08-05 21:35:11 +0300

    X

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ const App = () => {
    const postData = (e) => {
       console.log("data posted");
    };
+   async function logMovies() {
+      const response = await fetch("https://swapi.dev/api/people/1/");
+      const movies = await response.json();
+      console.log(movies);
+   }
 
    return (
       <div>
          <div>
             <h1>Post Data</h1>
             <button onClick={() => postData}>Click it</button>
+            <button onClick={logMovies}>Click Movies</button>
+            <button onClick={() => logMovies}>Click Movies Callback</button>
          </div>
       </div>
    );
